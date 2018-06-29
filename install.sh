5d80305927197e7e1b4f2941b9e92a3422482809
htouvet <htouvet-github@tranquil.it>
2018-06-29 18:13:55 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-#! /bin/sh
-apt-get install python conntrack net-utils
+#! /bin/bash
+apt-get install python python-iniparse conntrack net-tools arping 
 
 mkdir -p /usr/local/bin
 mkdir -p /opt/check_providers
 cp check_providers.py /opt/check_providers/check_providers.py
 chmod +x /opt/check_providers/check_providers.py
-ln -s /opt/check_providers/check_providers.py /usr/local/bin/check_providers
+ln -sb /opt/check_providers/check_providers.py /usr/local/bin/check_providers
 
-cp systemd/check_providers.service /lib/systemd/system/check_providers.service
-cp sample/check-providers.ini /etc/check-providers.ini
+cp -a systemd/check_providers.service /lib/systemd/system/check_providers.service
+cp -a samples/check-providers.ini /etc/check-providers.ini
 
+echo -------------------------------------
 echo Please modify configuration in /etc/check-providers.ini
-echo Then enable service with: 
+echo Then enable service with:
 echo     systemctl enable check_providers
-echo Check with 
+echo Check with
 echo     check_providers check
-echo monitor (ie check and enable/disable if necessary) with 
+echo monitor ie check and enable/disable if necessary with
 echo     check_providers monitor
 echo starts:
 echo     systemctl start check_providers
-
